b27db4220ec074ef9e0ab36217947843577cc7fa
Chiharu Mukai <49642982+MukaiC@users.noreply.github.com>
2020-04-26 12:42:18 +0200

    Added login and register routes, created login and register templates, added bootstrap classes

--- application.py
+++ application.py
@@ -22,8 +22,17 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
